f30f5c82a57a4a0726f66c2cb7de837e2218c59e
Max <shpax1@gmail.com>
2018-04-18 22:11:33 +0300

    refactor

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,31 @@
 const { leftMotor, rightMotor, batteryPin } = require('../models/vehicle');
 
+const interfaces = {};
+
 function setDriveSpeed(command) {
   const { lf = true, ls = 0, rf = true, rs = 0} = command;
 
   console.log('speed change', command);
 
-  leftMotor()[lf ? 'forward' : 'reverse'](ls);
-  rightMotor()[rf ? 'forward' : 'reverse'](rs);
+  interfaces.leftMotor[lf ? 'forward' : 'reverse'](ls);
+  interfaces.rightMotor[rf ? 'forward' : 'reverse'](rs);
 }
 
 function getBatteryLevel(addr) {
-  batteryPin().query(val => {
+  interfaces.batteryPin.query(val => {
     console.log('batt', val, addr);
   })
 }
 
-function mapEventsToVehicleActions(eventEmitter) {
+async function mapEventsToVehicleActions(eventEmitter) {
+  interfaces.leftMotor = await leftMotor();
+  interfaces.rightMotor = await rightMotor();
+  interfaces.batteryPin = await.batteryPin();
+
   eventEmitter.on('drive', setDriveSpeed);
   eventEmitter.on('battery', getBatteryLevel);
+
+  console.log("board interfaces ready");
 }
 
 module.exports = {
